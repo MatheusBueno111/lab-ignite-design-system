2821852bb5997ca2cf7b86108d1b7eba8108c713
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2022-10-13 15:14:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from './components/Text'
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div className="bg-cyan-500 font-bold text-xl text-violet-500">
+      Hello World
+      <Text />
+    </div>
+  )
+}
